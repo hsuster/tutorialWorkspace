77c03c4bddb5d9da45064bf26fdd1b68f0119a79
Andrew Hsu <andrewh@kvalinc.com>
2016-05-23 14:19:19 -0700

    Initial commit, turned first part of http://www.go4expert.com/articles/absolute-beginners-guide-wpf-t30091/ to calculator with window changing functionality

---
+++ ahTest1/CalcWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tutorialWorkspace
+{
+    /// <summary>
+    /// Interaction logic for CalcWindow.xaml
+    /// </summary>
+    public partial class CalcWindow : Window
+    {
+        public CalcWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_add_Click(object sender, RoutedEventArgs e)
+        {
+            int result = Int32.Parse(tb_num1.Text) + Int32.Parse(tb_num2.Text);
+            tb_result.Text = result.ToString();
+        }
+
+        private void button_sub_Click(object sender, RoutedEventArgs e)
+        {
+            int result = Int32.Parse(tb_num1.Text) - Int32.Parse(tb_num2.Text);
+            tb_result.Text = result.ToString();
+        }
+
+        private void button_mult_Click(object sender, RoutedEventArgs e)
+        {
+            int result = Int32.Parse(tb_num1.Text) * Int32.Parse(tb_num2.Text);
+            tb_result.Text = result.ToString();
+        }
+
+        private void button_div_Click(object sender, RoutedEventArgs e)
+        {
+            double result = float.Parse(tb_num1.Text) / float.Parse(tb_num2.Text + ".0");
+            tb_result.Text = result.ToString("0.00");
+        }
+
+        private void button_toMainWindow_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ ahTest1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tutorialWorkspace
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_toCalc_Click(object sender, RoutedEventArgs e)
+        {
+            CalcWindow calcWin = new CalcWindow();
+            calcWin.Show();
+            this.Close();
+        }
+
+        private void button_close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
